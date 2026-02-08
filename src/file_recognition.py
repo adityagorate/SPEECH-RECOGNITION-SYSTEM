cdd2218efd490cfa8d2b161ca841b133aae973b2
adityagorate <126947620+adityagorate@users.noreply.github.com>
2026-02-08 17:16:10 +0530

    Update file_recognition.py

--- src/file_recognition.py
+++ src/file_recognition.py
@@ -1,20 +1,21 @@
-import speech_recognition as sr
+import speech_recognition as sr    # import library
 
 def audio_file_to_text(file_path):
-    recognizer = sr.Recognizer()
+    recognizer = sr.Recognizer()        # create recognizer object
 
     try:
-        with sr.AudioFile(file_path) as source:
-            audio = recognizer.record(source)
+        with sr.AudioFile(file_path) as source:      # open audio file
+            audio = recognizer.record(source)      # read audio data
 
-        text = recognizer.recognize_google(audio)
-        return text
+        text = recognizer.recognize_google(audio)    # convert speech to text
+        return text       # return result
 
     except sr.UnknownValueError:
-        return "Error: Could not understand the audio"
+        return "Error: Could not understand the audio"      # audio not clear
 
     except sr.RequestError:
-        return "Error: Speech Recognition service unavailable"
+        return "Error: Speech Recognition service unavailable"         # API issue
 
     except FileNotFoundError:
-        return "Error: Audio file not found"
+
+        return "Error: Audio file not found"   # file missing
